ae2d253024b53a1f2c3b89a1bbe32fbeba52daaa
an-quodai <an.tran511@hotmail.com>
2023-06-19 16:13:12 +0700

    Revert "Revert "Edit model serialization""
    This reverts commit b7025b240b83c4cfe24ddc13e6ebcc561fc7b736.

--- app/Models/AppBaseModel.ts
+++ app/Models/AppBaseModel.ts
@@ -1,8 +1,5 @@
 import { Filterable } from '@ioc:Adonis/Addons/LucidFilter'
 import { BaseModel } from '@ioc:Adonis/Lucid/Orm'
-import { CamelCaseNamingStrategy } from 'App/Strategies/CamelCaseNamingStrategy'
 import { compose } from '@ioc:Adonis/Core/Helpers'
 
-export default class AppBaseModel extends compose(BaseModel, Filterable) {
-  public static namingStrategy = new CamelCaseNamingStrategy()
-}
+export default class AppBaseModel extends compose(BaseModel, Filterable) {}
